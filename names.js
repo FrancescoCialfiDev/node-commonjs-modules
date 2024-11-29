09c0c024b1bd596832e97d1d553cfa90dbb2e184
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2024-11-29 16:05:29 +0100

    Sse module.xports sintax for export function like a module.

--- names.js
+++ names.js
@@ -19,12 +19,16 @@ Buon lavoro ! :muscolo: (modificato)
 // Dovendo operae con dei moduli CommonJS, quindi senza l'utilizzo della nuova sintassi ES6 applichiamo anche qui 
 // i due valori di default di controllo del codice... ( Se avessimo usato estensione .mjs oppure type "module" nel file .json )
 // avremmo potuto evitare questa prassi poichè impostata di default
-console.log("ciao");
 
 "use strict"
-console.clear()
+console.clear();
+
+/*------Function-To-Generate-Name----------*/
 
 const generateName = function fullNameGenerator(fisrtName, lastName) {
-    return { fisrtName, lastName }
+    return { fisrtName, lastName };
 }
-console.log(generateName("Andrea", "Presti"))
+console.log(generateName("Andrea", "Presti"));
+
+/*------Modules-Export-Sintax----------*/
+module.exports = { generateName } // Questa sintassi ci permette di esportare piu valori.
